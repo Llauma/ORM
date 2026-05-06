14ecfa3dff9ada9a12d35c10c59bc49af2e5a994
Llauma <zuioro2004@gmail.com>
2026-05-06 18:13:25 +0300

    feat: add ButtonEntry for Cloth Config

---
+++ src/main/java/com/lauma/modmenu/ButtonEntry.java
@@ -0,0 +1,50 @@
+package com.lauma.modmenu;
+
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ButtonEntry extends AbstractConfigListEntry<Void> {
+
+    private final ButtonWidget button;
+
+    public ButtonEntry(Text label, Runnable action) {
+        super(label, false);
+        this.button = ButtonWidget.builder(label, b -> action.run())
+            .dimensions(0, 0, 150, 20)
+            .build();
+    }
+
+    @Override
+    public Void getValue() { return null; }
+
+    @Override
+    public Optional<Void> getDefaultValue() { return Optional.empty(); }
+
+    @Override
+    public void save() {}
+
+    @Override
+    public boolean isRequiresRestart() { return false; }
+
+    @Override
+    public List<? extends Element> children() { return List.of(button); }
+
+    @Override
+    public List<? extends Selectable> selectableChildren() { return List.of(button); }
+
+    @Override
+    public void render(DrawContext context, int index, int y, int x, int entryWidth,
+                       int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
+        button.setX(x + entryWidth / 2 - 75);
+        button.setY(y);
+        button.setWidth(150);
+        button.render(context, mouseX, mouseY, tickDelta);
+    }
+}
